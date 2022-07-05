5844dbda7618bc4f4f185ca947a798e6fc73751e
npankajaxioned <pankajn@axioned.com>
2022-07-05 12:53:17 +0530

    Changes made in js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,24 @@
-/* Author: 
-*/
-const contents = document.querySelectorAll('.slider li')
-const listitems = document.querySelectorAll('nav ul li a')
+var images = document.querySelectorAll('.images li');
+var listItem = document.querySelectorAll('.links li')
 
-console.log(contents, listitems);
-
-listitems.forEach((item, idx) => {
-    item.addEventListener('click', () => {
-        hideallcontents()
-        hideallitems()
-        
-        item.classList.add('active')
-        contents[idx].classList.add('active')
+listItem.forEach(function (li_item) {
+    // li_item.classList.remove('active')
+    li_item.addEventListener('click', function (e) {
+        var listItem_data = e.target.parentElement.dataset.filter;
+        images.forEach(function (item) {
+            var image_data = item.dataset.filter;
+            item.children[0].classList.remove('show')
+            if (listItem_data === image_data) {
+                item.children[0].classList.add('show')
+                remove_active();
+                li_item.classList.add('active');
+            }
+        })
     })
-})
+});
 
-function hideallcontents(){
-    contents.forEach(slider => slider.classList.remove('active'))
+function remove_active() {
+    listItem.forEach(function (item) {
+        item.classList.remove('active')
+    })
 }
-
-function hideallitems(){
-    listitems.forEach(item => item.classList.remove('active'))
-}
-
-
-
